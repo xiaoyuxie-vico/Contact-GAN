7ca6635dc986afe11a053c4c18616fe34a2a36d3
xiaoyu.xie <xiaoyuxie.vico@gmail.com>
2022-12-01 22:30:57 -0600

    update

---
+++ app.py
@@ -0,0 +1,215 @@
+# -*- coding: utf-8 -*-
+
+'''
+@author: Xiaoyu Xie
+@email: xiaoyuxie2020@u.northwestern.edu
+@date: Dec, 2022
+'''
+
+import io
+
+import matplotlib
+import matplotlib.pyplot as plt
+from matplotlib.pyplot import MultipleLocator
+import numpy as np
+import pandas as pd
+from scipy.linalg import svd
+import streamlit as st
+import seaborn as sns
+import mpld3
+import streamlit.components.v1 as components
+from mpld3 import plugins
+
+np.set_printoptions(precision=2)
+
+matplotlib.use('agg')
+
+
+def main():
+    apptitle = 'Contact Optimization via Mechanistic Machine Learning'
+    st.set_page_config(
+        page_title=apptitle,
+        page_icon=':eyeglasses:',
+        # layout='wide'
+    )
+    st.title('Contact Optimization via Mechanistic Machine Learning')
+    st.markdown('<p class="L2">- Developer: \nXiaoyu Xie, Northwestern University</p>', unsafe_allow_html=True)
+    st.markdown('<p class="L2">- Date: December, 2022.</p>', unsafe_allow_html=True)
+    st.image('images/schematic.png')
+
+    # level 1 font
+    st.markdown("""
+        <style>
+        .L1 {
+            font-size:40px !important;
+        }
+        </style>
+        """, 
+        unsafe_allow_html=True
+    )
+
+    # level 2 font
+    st.markdown("""
+        <style>
+        .L2 {
+            font-size:20px !important;
+        }
+        </style>
+        """, 
+        unsafe_allow_html=True
+    )
+    
+    #########################Objectives#########################
+    st.markdown('<p class="L1">Objectives</p>', unsafe_allow_html=True)
+    str_1 = '''1. Background: The contact region plays an important role in the convergence and accuracy of the simulation. IT IS EXTREMELY HARD TO GET THE SOLUTION CONVERGE.'''
+    st.markdown('<p class="L2">{}</p>'.format(str_1), unsafe_allow_html=True)
+
+    str_1 = '''2. Goal: To avoid trial and error, we propose a mechanistic machine learning-based inverse approach to optimize parameters for surface-to-surface contact'''
+    st.markdown('<p class="L2">{}</p>'.format(str_1), unsafe_allow_html=True)
+
+    #########################Data generation#########################
+    st.markdown('<p class="L1">Data Generation</p>', unsafe_allow_html=True)
+    str_1 = '''To generate a database, we applied Finite Element Analysis software (Abaqus) to generate simulation data with different concate confirguations.'''
+    st.markdown('<p class="L2">{}</p>'.format(str_1), unsafe_allow_html=True)
+    st.image('images/data_generation.png')
+
+    #########################Load dataset#########################
+    st.markdown('<p class="L1">Load dataset</p>', unsafe_allow_html=True)
+    st.markdown('<p class="L2">Upload simulation data</p>', unsafe_allow_html=True)
+
+    data_str_1 = 'Default dataset (different angles)'
+    data_str_2 = 'Default dataset (different loadings)'
+    flag = ['New dataset', data_str_1, data_str_2]
+    use_new_data = st.selectbox('Chosse a new dataset or use default dataset', flag, 1)
+
+    # load dataset
+    if use_new_data == 'New dataset':
+        uploaded_file = st.file_uploader('Choose a CSV file', accept_multiple_files=False)
+    
+    # load dataset
+    if use_new_data == 'New dataset':
+        data = io.BytesIO(uploaded_file.getbuffer())
+        df = pd.read_csv(data)
+    elif use_new_data == data_str_1:
+        file_path = 'dataset/dataset_different_angle.csv'
+        df = pd.read_csv(file_path)
+    elif use_new_data == data_str_2:
+        file_path = 'dataset/dataset_different_loading.csv'
+        df = pd.read_csv(file_path)
+    
+    st.markdown('Please upload your own dataset in a cvs file.')
+
+    #########################Data visualization#########################
+    df_norm = (df - df.mean()) / df.std()
+
+    st.markdown('<p class="L1">Data Visualization</p>', unsafe_allow_html=True)
+
+    st.markdown('Visualize the dataset:')
+    st.dataframe(df)
+    
+    st.markdown('Plot the dataset:')
+    if use_new_data == data_str_1:
+        fig = plt.figure()
+        ax = sns.lineplot(data=df, x='Operation_angle', y='Maximum_displacement', hue='Contact_length', marker='o')
+        plt.xlabel('Operation angle', fontsize=16)
+        plt.ylabel('Maximum displacement', fontsize=16)
+        plt.setp(ax.get_legend().get_texts(), fontsize='14')
+        plt.setp(ax.get_legend().get_title(), fontsize='16')
+        plt.tight_layout()
+        # st.pyplot(fig, clear_figure=True)
+        fig_html = mpld3.fig_to_html(fig)
+        components.html(fig_html, height=600)
+    elif use_new_data == data_str_2:
+        fig = plt.figure()
+        ax = sns.lineplot(data=df, x='Lifting_load', y='Maximum_displacement', hue='Contact_length', marker='o')
+        plt.xlabel('Lifting load', fontsize=16)
+        plt.ylabel('Maximum displacement', fontsize=16)
+        plt.setp(ax.get_legend().get_texts(), fontsize='14')
+        plt.setp(ax.get_legend().get_title(), fontsize='16')
+        plt.tight_layout()
+        # st.pyplot(fig, clear_figure=True)
+        fig_html = mpld3.fig_to_html(fig)
+        components.html(fig_html, height=600)
+
+    #########################Model training#########################
+    st.markdown('<p class="L1">Mechanistic Machine Learning Model Training</p>', unsafe_allow_html=True)
+    st.image('images/model.png')
+
+    st.markdown('**Hyperparameters:**')
+    st.markdown('- The maximum decision tree depth is 3.')
+    st.markdown('- The number of trees is 100.')
+    st.markdown('- The loss function is mean squared error.')
+    st.markdown('- The minimum number of samples required to split an internal node is 2.')
+
+    from sklearn.model_selection import train_test_split
+    from sklearn.ensemble import RandomForestRegressor
+
+    df_train, df_test = train_test_split(df_norm, test_size=0.2, random_state=5)
+    if use_new_data == data_str_1:
+        X_train, y_train = df_train[['Contact_length', 'Operation_angle']], df_train[['Maximum_displacement']]
+        X_test, y_test = df_test[['Contact_length', 'Operation_angle']], df_test[['Maximum_displacement']]
+    elif use_new_data == data_str_2:
+        X_train, y_train = df_train[['Contact_length', 'Lifting_load']], df_train[['Maximum_Mises_stress', 'Maximum_displacement']]
+        X_test, y_test = df_test[['Contact_length', 'Lifting_load']], df_test[['Maximum_Mises_stress', 'Maximum_displacement']]
+
+    regr = RandomForestRegressor(max_depth=3).fit(X_train, y_train)
+
+    pred_test = regr.predict(X_test)
+
+    r2_train = regr.score(X_train, y_train)
+    r2_test = regr.score(X_test, y_test)
+
+    st.markdown('**Start training a mechanistic machine learning model:**')
+    st.markdown('**Training results:**')
+    st.markdown('- R2 score in the training set is **{:.4f}**.'.format(r2_train))
+    st.markdown('- R2 score in the test set is **{:.4f}**.'.format(r2_test))
+
+    #########################Design guidance#########################
+    st.markdown('<p class="L1">Design guidance</p>', unsafe_allow_html=True)
+
+    X_target_list = []
+
+    if use_new_data == data_str_1:
+        for i in range(45, 55):
+            X_target_list.append([(i - df.mean()[0]) / df.std()[0], 0])
+    elif use_new_data == data_str_2:
+        for i in range(45, 65):
+            X_target_list.append([(i - df.mean()[0]) / df.std()[0], 0.1])
+
+    X_target = np.array(X_target_list)
+    print(X_target.shape)
+    pred_X_target = regr.predict(X_target)
+    pred_y_target = pred_X_target * df.std()[2] + df.mean()[2]
+
+    if use_new_data == data_str_1:
+        st.markdown("**Choose ideal contact length based on users' preferences on the maximum displacement.**")
+        fig = plt.figure()
+        plt.plot(X_target[:,0] * df.std()[0] + df.mean()[0], pred_y_target)
+        plt.scatter(X_target[:,0] * df.std()[0] + df.mean()[0], pred_y_target)
+        plt.xlabel('Contact length', fontsize=16)
+        plt.ylabel('Maximum_displacement', fontsize=16)
+        fig_html = mpld3.fig_to_html(fig)
+        components.html(fig_html, height=600)
+    
+    if use_new_data == data_str_2:
+        st.markdown("**Choose ideal contact length based on users' preferences on the maximum Mises stress.**")
+        fig = plt.figure()
+        plt.plot(X_target[:,0] * df.std()[0] + df.mean()[0], pred_y_target[:,0])
+        plt.scatter(X_target[:,0] * df.std()[0] + df.mean()[0], pred_y_target[:,0])
+        plt.xlabel('Contact length', fontsize=16)
+        plt.ylabel('Maximum_Mises_stress', fontsize=16)
+        fig_html = mpld3.fig_to_html(fig)
+        components.html(fig_html, height=600)
+        
+        st.markdown("**Choose ideal contact length based on users' preferences on the maximum displacement.**")
+        fig = plt.figure()
+        plt.plot(X_target[:,0] * df.std()[0] + df.mean()[0], pred_y_target[:,1])
+        plt.scatter(X_target[:,0] * df.std()[0] + df.mean()[0], pred_y_target[:,1])
+        plt.xlabel('Contact length', fontsize=16)
+        plt.ylabel('Maximum_displacement', fontsize=16)
+        fig_html = mpld3.fig_to_html(fig)
+        components.html(fig_html, height=600)
+
+
+if __name__ == '__main__':
+    main()
---
+++ mass_spring_app_back.py
@@ -0,0 +1,228 @@
+# -*- coding: utf-8 -*-
+
+'''
+@author: Xiaoyu Xie
+@email: xiaoyuxie2020@u.northwestern.edu
+@date: Sep, 2021
+'''
+
+import io
+
+import matplotlib
+import matplotlib.pyplot as plt
+from matplotlib.pyplot import MultipleLocator
+import numpy as np
+import pandas as pd
+from scipy.linalg import svd
+import streamlit as st
+
+
+np.set_printoptions(precision=2)
+# plt.rcParams["font.family"] = "Arial"
+
+
+matplotlib.use('agg')
+
+
+def main():
+    apptitle = 'Mass-Spring-Damper System Analysis'
+    st.set_page_config(
+        page_title=apptitle,
+        page_icon=':eyeglasses:',
+        # layout='wide'
+    )
+    st.title('Dimension reduction for spring-mass-damping system')
+
+    # level 1 font
+    st.markdown("""
+        <style>
+        .L1 {
+            font-size:40px !important;
+        }
+        </style>
+        """, 
+        unsafe_allow_html=True
+    )
+
+    # level 2 font
+    st.markdown("""
+        <style>
+        .L2 {
+            font-size:20px !important;
+        }
+        </style>
+        """, 
+        unsafe_allow_html=True
+    )
+    #########################Objectives#########################
+
+    st.markdown('<p class="L1">Objectives</p>', unsafe_allow_html=True)
+
+    str_1 = '''1. Illustrate the motion of the mass with time captured by camera.'''
+    str_2 = '''2. Reduce the dimension of the camera data and identify the intrinsic dimension by principal component analysis (PCA).'''
+    # str_3 = '''3. Identify governing physical parameters for the spring mass system (natural frequency, mass, spring constant, damping, etc.'''
+
+    st.markdown('<p class="L2">{}</p>'.format(str_1), unsafe_allow_html=True)
+    st.markdown('<p class="L2">{}</p>'.format(str_2), unsafe_allow_html=True)
+    # st.markdown('<p class="L2">{}</p>'.format(str_3), unsafe_allow_html=True)
+
+    #########################Experimental set#########################
+
+    # Experimental set
+
+    st.markdown('<p class="L1">Experimental set</p>', unsafe_allow_html=True)
+    st.image('src/schematic.png')
+
+    st.markdown('<p class="L2">Videos:</p>', unsafe_allow_html=True)
+    str_1 = """[1. Camera 1](https://drive.google.com/file/d/1-BukVXmKl5G-hmR5v1dCUw7tEUy0MXAl/view?usp=sharing)"""
+    st.markdown(str_1)
+    str_2 = """[2. Camera 2](https://drive.google.com/file/d/1qTe8MC7yvCOlFx2JDDX6W5rvmHKfRCYI/view?usp=sharing)"""
+    st.markdown(str_2)
+    str_3 = """[3. Camera 3](https://drive.google.com/file/d/1NNwmqHz6wA6kToc6Ydqvs_1Mb7yZjvZX/view?usp=sharing)"""
+    st.markdown(str_3)
+
+    #########################Dataset#########################
+    st.markdown('<p class="L1">Upload motion data from different cameras</p>', unsafe_allow_html=True)
+
+    flag = ['New dataset', 'Default dataset']
+    use_new_data = st.selectbox('Chosse a new dataset or use default dataset', flag, 1)
+
+    # load dataset
+    if use_new_data == 'New dataset':
+        uploaded_file = st.file_uploader('Choose a CSV file', accept_multiple_files=False)
+    
+    # button_dataset = st.button('Click once you have selected a dataset')
+    # if button_dataset:
+    # load dataset
+    if use_new_data == 'New dataset':
+        data = io.BytesIO(uploaded_file.getbuffer())
+        df = pd.read_csv(data)
+    elif use_new_data == 'Default dataset':
+        file_path = 'src/multidata2.csv'
+        df = pd.read_csv(file_path)
+
+    lines = ['Camera 1', 'Camera 2', 'Camera 3']
+    chosen_line = st.selectbox('Select a camera to show raw data', lines, 0)
+    chosen_line = chosen_line.replace('Camera ', '')
+    global ratio
+    ratio = st.slider('Select a ratio to show raw data', 0.01, 1.0, 1.0)
+
+    col1, col2, col3 = st.beta_columns(3)
+    show_length = int(df.shape[0] * ratio)-1
+    with col1:
+        fig = plt.figure()
+        plt.plot(df['t'][:show_length], df[f'x{chosen_line}'][:show_length])
+        plt.xlabel('t', fontsize=30)
+        plt.ylabel(f'x', fontsize=30)
+        plt.title(f'x (Camera {chosen_line})', fontsize=34)
+        plt.tick_params(labelsize=26)
+        plt.tight_layout()
+        st.pyplot(fig, clear_figure=True)
+    with col2:
+        fig = plt.figure()
+        plt.plot(df['t'][:show_length], df[f'y{chosen_line}'][:show_length])
+        plt.xlabel('t', fontsize=30)
+        plt.ylabel(f'y', fontsize=30)
+        plt.title(f'y (Camera {chosen_line})', fontsize=34)
+        plt.tick_params(labelsize=26)
+        plt.tight_layout()
+        st.pyplot(fig, clear_figure=True)
+    with col3:
+        fig = plt.figure()
+        plt.scatter(df[f'x{chosen_line}'][:show_length], df[f'y{chosen_line}'][:show_length])
+        plt.xlabel(f'x', fontsize=30)
+        plt.ylabel(f'y', fontsize=30)
+        plt.title(f'x-y (Camera {chosen_line})', fontsize=34)
+        plt.tick_params(labelsize=26)
+        plt.tight_layout()
+        st.pyplot(fig, clear_figure=True)
+
+    # st.markdown('Some statistics:')
+    # st.dataframe(df.describe())
+
+    # basic information for this dataset
+    df_info = df.describe()
+
+    #########################PCA#########################
+    # subtract mean for each column
+    for col_name in df.columns:
+        if col_name == 't':
+            continue
+        df['{}_new'.format(col_name)] = df[col_name] - \
+            df_info[col_name]['mean']
+
+    df_part = df[['t', 'x1_new', 'y1_new',
+                  'x2_new', 'y2_new', 'x3_new', 'y3_new']]
+
+    # original data: m*N (m: data point number, N: dimension for each data)
+    X = df[['x1', 'y1', 'x2', 'y2', 'x3', 'y3']].to_numpy()
+
+    # transpose X as B
+    B = df[['x1_new', 'y1_new', 'x2_new',
+            'y2_new', 'x3_new', 'y3_new']].to_numpy().T
+
+    # SVD
+    U, s, VT = svd(B, full_matrices=0)
+
+    # Transform B to Y
+    Y = U.T.dot(B)
+
+    st.markdown('<p class="L1">Dimension reduction by principal component analysis (PCA)</p>', unsafe_allow_html=True)
+
+    # st.markdown('## Principal component analysis (PCA)')
+    # st.markdown('''
+    # ```
+    # # original data: m*N (m: data point number, N: dimension for each data)
+    # X = df[['xa', 'ya', 'xb', 'yb', 'xc', 'yc']].to_numpy()
+
+    # # transpose X as B
+    # B = df[['x1_new', 'y1_new', 'x2_new', 'y2_new', 'x3_new', 'y3_new']].to_numpy().T
+
+    # # SVD
+    # U, s, VT = svd(B, full_matrices=0)
+    # ```
+    # ''')
+    eigenvalues = s**2/(df.shape[0]-1)
+    eigenvalues_str = ', '.join([str(round(i, 2)) for i in eigenvalues.tolist()])
+    st.markdown('<p class="L2">Eigenvalues:</p>', unsafe_allow_html=True)
+    st.markdown(f'[{eigenvalues_str}]')
+
+    x_major_locator = MultipleLocator(1)  # used to set x-axis tick interval
+
+    # plot the log of eigenvalues
+    fig = plt.figure()
+    ax1 = fig.add_subplot(1, 1, 1)
+    # ax1.semilogy(range(1, 7), s, '-o', color='k')
+    ax1.plot(range(1, 7), eigenvalues, '-o', color='k')
+    ax1.xaxis.set_major_locator(x_major_locator)  # set x-axis tick interval
+    ax1.set_xlabel('Principal Component Number', fontsize=16)
+    ax1.set_ylabel('Eigenvalues', fontsize=16)
+    plt.tight_layout()
+    plt.grid()
+    st.pyplot(fig, clear_figure=True)
+
+    st.markdown('## This system is one-dimensional.', unsafe_allow_html=True)
+
+    #########################ODE#########################
+    st.markdown('<p class="L1">Recover ordinary differential equation</p>',
+                unsafe_allow_html=True)
+    #recover ODE
+    t = df['t']
+    z = B[:, 1]
+    Z = np.stack((z), axis=-1)  # First column is x, second is y
+    #print(X)
+
+    #plot x
+    fig = plt.figure(figsize=(8, 6))
+    ax = fig.add_subplot(111)
+    ax.plot(t, z, '-', color='blue', label='z')
+    ax.set_xlabel("Time (s)", fontsize=20)
+    ax.tick_params(labelsize=20)
+    ax.legend(fontsize=20)
+    #ax.set_xlim(-0.05, 1.05)
+    #ax.set_ylim(-6.5, 2.5)
+    st.pyplot(fig, clear_figure=True)
+
+
+if __name__ == '__main__':
+    main()
